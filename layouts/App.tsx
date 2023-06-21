58b1de1735293d73ed512757e3115f8edf627992
jake.kyo <jake.kyo@kakaocorp.com>
2023-06-21 16:20:56 +0900

    코드 스플리팅과 CSS 이모션 적용

--- layouts/App.tsx
+++ layouts/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import loadable from '@loadable/component';
 
 // Pages
-import LogIn from '@pages/LogIn';
-import SignUp from '@pages/SignUp';
+const LogIn = loadable(() => import('@pages/LogIn'));
+const SignUp = loadable(() => import('@pages/SignUp'));
 
 const App = () => {
   return (
